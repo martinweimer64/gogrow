4fa209ee7a266c09283966394add792f9294b5b7
martinweimer64 <martinweimer92@gmail.com>
2022-08-31 00:51:15 -0300

    added delay to action

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,10 +1,12 @@
 class AttachmentsController < ApplicationController
   def upload_sales
-    result = SaveFromFile.call(base_64_file: params[:base_64_file])
-    if result.success?
-      render json: result.success_with_errors, status: 200
-    else
-      render json: {error: result.message}, status: :unprocessable_entity
-    end
+    result = SaveFromFile.delay.call(base_64_file: params[:base_64_file])
+    render json: result
+    #To use without delay
+    #if result.success?
+      #render json: result.success_with_errors, status: 200
+    #else
+      #render json: {error: result.message}, status: :unprocessable_entity
+    #end
   end
 end
